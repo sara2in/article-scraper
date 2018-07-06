cff1610380ab427b46641bc24dcd418752acbb76
Salem Ezz <sal@Salems-MacBook-Pro.local>
2018-07-06 17:12:25 -0400

    mongodb_uri

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ var db = require("./models");
 
 var PORT = 3000;
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 // Initialize Express
 var app = express();
 
